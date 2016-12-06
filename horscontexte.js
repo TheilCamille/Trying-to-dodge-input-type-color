43f01c192184ad775d546d35c67fcce96355a03a
TheilCamille <camille@theil.fr>
2016-12-06 22:49:24 +0100

    Update horscontexte.js

--- horscontexte.js
+++ horscontexte.js
@@ -1,3 +1,5 @@
+function myRange() {
+
     // Get value of input type range
     var range1 = document.getElementById("rgb1").value;
     var range2 = document.getElementById("rgb2").value;
@@ -10,24 +12,34 @@
     var label3 = document.getElementById("lb3");
     var label4 = document.getElementById('lb4');
 
+    // Change the label value for the input range value
+    label1.innerHTML= range1;
+    label2.innerHTML= range2;
+    label3.innerHTML= range3;
+    label4.innerHTML= range4;
+}
 
 // We call the function for the initial label value
 myRange();
 
 function changeColor() {
-        // Change the background color of the second <section> with the rgba value of range input
+
+    // Get value of input type range (again)
+    var range1 = document.getElementById("rgb1").value;
+    var range2 = document.getElementById("rgb2").value;
+    var range3 = document.getElementById("rgb3").value;
+    var range4 = document.getElementById("rgb4").value;
+
+    // Change the background color of the second <section> with the rgba value of range input
     document.getElementById('showMe').style.background = "rgba(" + range1 + "," + range2 + "," + range3 +"," + range4 + ")";
 }
 
 function generateCode() {
-        // Change html of the third <section> with the css code of a background-color change with rgba value
-    document.getElementById('showMeCode').innerHTML= "background-color: rgba(" + range1 + "," + range2 + "," + range3 +"," + range4 + ")";
-}
 
-function myRange() {
-        // Change the label value for the input range value
-    label1.innerHTML= range1;
-    label2.innerHTML= range2;
-    label3.innerHTML= range3;
-    label4.innerHTML= range4;
+    var range1 = document.getElementById("rgb1").value;
+    var range2 = document.getElementById("rgb2").value;
+    var range3 = document.getElementById("rgb3").value;
+    var range4 = document.getElementById("rgb4").value;
+
+    document.getElementById('showMeCode').innerHTML= "background-color: rgba(" + range1 + "," + range2 + "," + range3 +"," + range4 + ")";
 }
