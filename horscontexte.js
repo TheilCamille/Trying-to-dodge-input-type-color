07c27dd5e5e746548bcdd95ce69cc8f79e6d5240
TheilCamille <camille@theil.fr>
2016-12-06 20:35:46 +0100

    Update horscontexte.js

--- horscontexte.js
+++ horscontexte.js
@@ -1,24 +1,3 @@
-function myRange() {
-
-    // Get value of input type range
-    var range1 = document.getElementById("rgb1").value;
-    var range2 = document.getElementById("rgb2").value;
-    var range3 = document.getElementById("rgb3").value;
-    var range4 = document.getElementById("rgb4").value;
-
-    // Get the label
-    var label1 = document.getElementById("lb1");
-    var label2 = document.getElementById("lb2");
-    var label3 = document.getElementById("lb3");
-    var label4 = document.getElementById('lb4');
-
-    // Change the label value for the input range value
-    label1.innerHTML= range1;
-    label2.innerHTML= range2;
-    label3.innerHTML= range3;
-    label4.innerHTML= range4;
-}
-
 // We call the function for the initial label value
 myRange();
 
@@ -42,4 +21,25 @@ function generateCode() {
     var range4 = document.getElementById("rgb4").value;
 
     document.getElementById('showMeCode').innerHTML= "background-color: rgba(" + range1 + "," + range2 + "," + range3 +"," + range4 + ")";
+}
+
+function myRange() {
+
+    // Get value of input type range
+    var range1 = document.getElementById("rgb1").value;
+    var range2 = document.getElementById("rgb2").value;
+    var range3 = document.getElementById("rgb3").value;
+    var range4 = document.getElementById("rgb4").value;
+
+    // Get the label
+    var label1 = document.getElementById("lb1");
+    var label2 = document.getElementById("lb2");
+    var label3 = document.getElementById("lb3");
+    var label4 = document.getElementById('lb4');
+
+    // Change the label value for the input range value
+    label1.innerHTML= range1;
+    label2.innerHTML= range2;
+    label3.innerHTML= range3;
+    label4.innerHTML= range4;
 }
