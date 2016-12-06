2e93299bb470c4dd827794c2e9fe6c5d846cba64
TheilCamille <camille.theil@gmail.com>
2016-12-06 17:09:37 +0100

    Merci pat !

---
+++ horscontexte.js
@@ -0,0 +1,45 @@
+function myRange() {
+
+    // Get value of input type range
+    var range1 = document.getElementById("rgb1").value;
+    var range2 = document.getElementById("rgb2").value;
+    var range3 = document.getElementById("rgb3").value;
+    var range4 = document.getElementById("rgb4").value;
+
+    // Get the label
+    var label1 = document.getElementById("lb1");
+    var label2 = document.getElementById("lb2");
+    var label3 = document.getElementById("lb3");
+    var label4 = document.getElementById('lb4');
+
+    // Change the label value for the input range value
+    label1.innerHTML= range1;
+    label2.innerHTML= range2;
+    label3.innerHTML= range3;
+    label4.innerHTML= range4;
+}
+
+// We call the function for the initial label value
+myRange();
+
+function changeColor() {
+
+    // Get value of input type range (again)
+    var range1 = document.getElementById("rgb1").value;
+    var range2 = document.getElementById("rgb2").value;
+    var range3 = document.getElementById("rgb3").value;
+    var range4 = document.getElementById("rgb4").value;
+
+    // Change the background color of the second <section> with the rgba value of range input
+    document.getElementById('showMe').style.background = "rgba(" + range1 + "," + range2 + "," + range3 +"," + range4 + ")";
+}
+
+function generateCode() {
+
+    var range1 = document.getElementById("rgb1").value;
+    var range2 = document.getElementById("rgb2").value;
+    var range3 = document.getElementById("rgb3").value;
+    var range4 = document.getElementById("rgb4").value;
+
+    document.getElementById('showMeCode').innerHTML= "background-color: rgba(" + range1 + "," + range2 + "," + range3 +"," + range4 + ")";
+}
